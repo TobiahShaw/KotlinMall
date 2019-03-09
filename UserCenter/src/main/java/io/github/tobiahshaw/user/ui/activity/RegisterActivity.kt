00d3e8246504bfafdfd926b41bee6572ab8473c0
TobiahShaw <a5414994@outlook.com>
2019-03-09 18:30:35 +0800

    usage of kotlin android extensions

--- UserCenter/src/main/java/io/github/tobiahshaw/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/io/github/tobiahshaw/user/ui/activity/RegisterActivity.kt
@@ -2,12 +2,17 @@ package io.github.tobiahshaw.user.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import io.github.tobiahshaw.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegisterBtn.setOnClickListener {
+            Toast.makeText(this, "register", Toast.LENGTH_SHORT).show()
+        }
     }
 }
