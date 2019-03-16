3cb2f296e81a5afef83bfaaebbeb6b15a89048a2
TobiahShaw <a5414994@outlook.com>
2019-03-16 23:28:21 +0800

    add progress dialog

---
+++ BaseLibrary/src/main/java/io/github/tobiahshaw/base/widgets/ProgressLoading.kt
@@ -0,0 +1,41 @@
+package io.github.tobiahshaw.base.widgets
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.view.Gravity
+import android.widget.ImageView
+import io.github.tobiahshaw.base.R
+import org.jetbrains.anko.find
+
+class ProgressLoading(context: Context, theme: Int) : Dialog(context, theme) {
+
+    companion object {
+        private lateinit var mDialog: ProgressLoading
+
+        private var animDrawable: AnimationDrawable? = null
+
+        fun create(context: Context) {
+            mDialog = ProgressLoading(context, R.style.LightProgressDialog)
+            mDialog.setContentView(R.layout.progress_dialog)
+            mDialog.setCancelable(true)
+            mDialog.setCanceledOnTouchOutside(false)
+            mDialog.window?.attributes!!.gravity = Gravity.CENTER
+            mDialog.window?.attributes!!.dimAmount = 0.2f
+
+            val loading = mDialog.find<ImageView>(R.id.iv_loading)
+            animDrawable = loading.background as AnimationDrawable
+        }
+    }
+
+    override fun show() {
+        super.show()
+        animDrawable?.start()
+    }
+
+    fun hideLoading() {
+        dismiss()
+        animDrawable?.stop()
+    }
+
+}
